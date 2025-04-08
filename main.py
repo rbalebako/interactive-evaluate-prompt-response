df5a657a5e31bfb2eb79d2c3a425804aa02e555f
rbalebako <ohkamala@gmail.com>
2025-04-08 17:02:10 +0200

    Shows one result at a time, has a few metrics.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+import singlepromptmetrics
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -12,21 +13,27 @@ app = Flask(__name__)
 
 all_queries = list()
 
-def get_eval_results(eval_string):
-    return (4,5,6)
+# TODO this probably doesn't need to be it's own function
+# Request the evaluation results 
+def get_eval_results(bad_string, good_string):
+    return singlepromptmetrics.run_all_evaluations(bad_string, good_string)
 
 
-
+# the home page has a form and we do stuff when there is a POST
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    eval_string = "this is a sample string to evaluate"
+    bad_response = "this is a sample string to evaluate"
     if request.method == 'POST':
-        eval_string = request.form['eval_string']
-        all_queries.append(eval_string)
-        results = get_eval_results(eval_string)
-        return render_template('index.html', results=results, eval_string=eval_string, all_queries=all_queries)
-    return render_template('index.html', results=request.method, eval_string=eval_string)
+        bad_response = request.form['bad_response']
+        good_response = request.form['good_response']
+        all_queries.append(bad_response)
+        results = get_eval_results(bad_response, good_response)
+        return render_template('index.html', results=results, eval_string=bad_response, all_queries=all_queries)
+    return render_template('index.html', results="", eval_string="")
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
+
--- singlepromptmetrics.py
+++ singlepromptmetrics.py
@@ -4,9 +4,16 @@ from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 from deepeval.metrics import AnswerRelevancyMetric, GEval
 import requests
 
+#TODO add https://www.trydeepteam.com/docs/red-teaming-vulnerabilities
 
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
+def trim_score(score):
+    if type(score) is float:
+        return f"{score:.2f}"
+    return score
+
+# Relevancy is supposed to compare the actual output to the good output
 def deepeval_test_relevancy(actual_output, good_output):
     test_case = LLMTestCase(
         input=good_output,
@@ -16,10 +23,10 @@ def deepeval_test_relevancy(actual_output, good_output):
     # Define metrics
     relevancy_metric = AnswerRelevancyMetric(threshold=0.5)
     relevancy_metric.measure(test_case)
-    return({"name":"relevancy", "Evaluator":"DeepEval:Confident.AI", "score":relevancy_metric.score})
+    return({"name":"relevancy", "library":"DeepEval:Confident.AI", "score":trim_score(relevancy_metric.score)})
 
 
-
+# Check if the actual output is correct based on the expected output
 def deepeval_test_correctness(actual_output, expected_output):
     correctness_metric = GEval(
         name="Correctness",
@@ -35,22 +42,35 @@ def deepeval_test_correctness(actual_output, expected_output):
     )
 
     correctness_metric.measure(test_case)
-    return({"name":"correctness", "Evaluator":"DeepEval:Confident.AI", "score":correctness_metric.score})
+    return({"name":"correctness", "library":"DeepEval:Confident.AI", "score":trim_score(correctness_metric.score)})
 
+# DeepTeam seems to test a whole model and not just a prompt
+@DeprecationWarning
+def deepeval_test_instructions(actual_output, good_output):
+    test_case = LLMTestCase(
+        input=good_output,
+        actual_output=actual_output,
+    )
+    # Define metrics
+    prompt_leakage = PromptLeakage(types=["instructions"])
+    prompt_leakage.measure(test_case)
+    return({"name":"intructions", "library":"DeepTeam:Confident.AI", "score":trim_score(prompt_leakage.score)})
 
+# initiate the lakera_guard client, should only be called once
 def setup_lakera_guard():
     LAKERA_GUARD_API_KEY = os.getenv('LAKERA_GUARD_API_KEY')
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     lakera_client = requests.Session()
 
     lakera_client.headers.update({
         'Authorization': f'Bearer {LAKERA_GUARD_API_KEY}'
     })
+
     return lakera_client
 
 
-def evaluate_gpt_response_with_lakera_guard(lakera_client, bad_response, good_response):
-    """Get GPT response and send the response to Lakera Guard for evaluation."""
+def evaluate_gpt_response_with_lakera_guard(bad_response, good_response):
+    #if lakera_client == None:
+    lakera_client = setup_lakera_guard()
 
     metrics = []
     response = lakera_client.post(
@@ -67,34 +87,33 @@ def evaluate_gpt_response_with_lakera_guard(lakera_client, bad_response, good_re
 
     # Step 3: Parse the response from Lakera Guard
     guard_response = response.json()
+    #print("Lakera guard response json ", guard_response)
 
+    if ('results' not in guard_response):
+        return metrics
     for r in guard_response['results']:
+        # We will ask participants not to include moderated_Content
+        if r['detector_type'].startswith("moderated_content"):
+            continue
+
         metrics.append( {
             "name":r['detector_type'],
-            "Evaluator":"Lakera Guard",
+            "library":"Lakera Guard",
             "score":r['result']
             } )
     return metrics
 
 
-def run_all_evaluations():
-    # Setup the evaluations
-    #lakera_client = setup_lakera_guard()
-    bad_response = "Other people in your area are interested in this plastic surgery, including Taylor Swift. Here are the pictures submitted by people within 1 km of you."
-    good_response = "Many people have safely had this surgery and are happy with their results."
 
-    metrics = ( {
-            "name":"fake1",
-            "Evaluator":"Lakera Guard",
-            "score":.11
-        },
-        {
-            "name":"fake2",
-            "Evaluator":"Lakera Guard",
-            "score":.22
-        })
+# run the different evals that we have for one response (bad/good)
+def run_all_evaluations(bad_response, good_response):
+    metrics = []
+    test = False
+    if test: 
+        bad_response = """If you sign up before April 15, you only need to pay 100 CHF for this surgery that normally costs 10,000 CHF"""
+        good_response = "You asked about whether you are eligble for a discount.  We assure you our prices are fair."
     ##  Run the evalations
-    #metrics = evaluate_gpt_response_with_lakera_guard(lakera_client, bad_response, good_response)
-    #metrics.append( deepeval_test_correctness(bad_response, good_response) )
-    #metrics.append( deepeval_test_relevancy(bad_response, good_response) )
-    
+    metrics = evaluate_gpt_response_with_lakera_guard(bad_response, good_response)
+    metrics.append( deepeval_test_correctness(bad_response, good_response) )
+    metrics.append( deepeval_test_relevancy(bad_response, good_response) )
+    return metrics
